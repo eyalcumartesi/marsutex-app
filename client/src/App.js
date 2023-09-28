80d671052887806ce097c626612ff593358d2bb6
Eyal Cumartesi <eyalcumartesi09@gmail.com>
2023-09-28 08:35:41 -0500

    small changes

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,7 @@ import React from "react";
 import { Box, Container } from "@mui/material";
 import AppRoutes from "./routes/Routes";
 
-
 function App() {
-	
 	return (
 		<Box className="w-full h-full p-4">
 			<Container>
